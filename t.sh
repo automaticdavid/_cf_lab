ca6183b1ba92d52e128e4c1be0df5a2a83fc309d
root <root@workstation-f600.rhpds.opentlc.com>
2018-02-21 05:28:36 -0500

    sync

--- t.sh
+++ t.sh
@@ -1,5 +1,5 @@
 
-subscription-manager register --auto-attach
+# subscription-manager register --auto-attach
 yum install redhat-access-insights
 redhat-access-insights --register
 
@@ -15,8 +15,16 @@ yum install git
          
 git clone https://github.com/adamgoossens/cf-ansible.git cloudforms
  
+ansible-galaxy install -r requirements.yml
+
+ 
 # ansible
+ ansible-playbook -i hosts/ -e @vault.yml --ask-vault-pass playbook.yml --skip-tags updates --tags new -v
 
 # register cloudforms appliance
+
+
+subscription-manager register --activationkey=rhn-vs-ehrman --org=5318211
+
 
 # Do CF zones 
