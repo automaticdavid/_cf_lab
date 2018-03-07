57c5c9f383e291a61e45b6ea3fe079c643f303be
root <root@workstation-2db3.rhpds.opentlc.com>
2018-03-07 07:33:26 -0500

    sync

--- t1.sh
+++ t1.sh
@@ -16,8 +16,11 @@ yum -y install ansible
 # yum -y install git
 # yum -y install atomic-openshift-clients
 
-export LD_LIBRARY_PATH=/opt/rh/python27/root/usr/lib64
-/opt/rh/python27/root/usr/bin/pip install git+https://github.com/ManageIQ/manageiq-api-client-python.git
+# export LD_LIBRARY_PATH=/opt/rh/python27/root/usr/lib64
+# /opt/rh/python27/root/usr/bin/pip install git+https://github.com/ManageIQ/manageiq-api-client-python.git
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install git+https://github.com/ManageIQ/manageiq-api-client-python.git
 
 ansible-galaxy install -r requirements.yml
 
@@ -28,10 +31,6 @@ for host in $hostlist; do ssh-copy-id -o StrictHostKeyChecking=false $host;done
 cp /root/VMware-vix-disklib-5.5.0-1284542.x86_64.tar.gz ./files/VMware-vix-disklib-5.5.0-1284542.x86_64.tar.gz
 
 echo "CHANGE LAB ID IN VARS"
+echo "CREATE VAULT"
 
-
-
-ansible-playbook -i hosts/ -e @vault.yml --ask-vault-pass upgrade.yml -vvv
-ansible-playbook -i hosts/ -e @vault.yml --ask-vault-pass pre-zone.yml --skip-tags updates -v
-ansible-playbook -i hosts/ -e @vault.yml --ask-vault-pass post-zone.yml --skip-tags updates 
 
--- t2.sh
+++ t2.sh
@@ -5,3 +5,15 @@
 ansible-playbook -i hosts/ -e @vault.yml --ask-vault-pass upgrade.yml -vvv
 ansible-playbook -i hosts/ -e @vault.yml --ask-vault-pass pre-zone.yml --skip-tags updates -v
 ansible-playbook -i hosts/ -e @vault.yml --ask-vault-pass post-zone.yml --skip-tags updates
+
+# def providers: rhev, tower, aws
+
+# Tower: 
+# get license
+# create team
+# create project
+# create cred for aws api
+# create dyn inventory for cf + groups
+# create template
+
+
